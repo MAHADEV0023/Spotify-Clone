545ae29c8eea9c1a4754db38fa3c18ebd9fa9590
Mahadev . <mahadev636308@gmail.com>
2024-07-21 16:45:21 +0530

    Add files via upload
    fist commit

---
+++ Spotify Project/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("load", function() {
+        var preloader = document.getElementById('preloader');
+        preloader.style.display = 'none';
+    });
+});
